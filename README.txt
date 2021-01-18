403fd0adb5ebde1f8de054dbba296f7fd532ddbe
shh0884 <51844838+shh0884@users.noreply.github.com>
2021-01-18 15:32:31 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,11 @@
+README version 2.0
+
+Title: FrameWhite.
+Main source: manifest.json, popup.html, code.js, README.txt.
+Description: 
+Enjoy FrameWhite, FrameBlack and LightBlue, LightGreen, LightYellow, LightRed themes.
+
+
+From Sungho Hwang.
+
+December 8th 2020.
---
+++ code.js
@@ -0,0 +1,221 @@
+formOne.addEventListener('click', function () {
+var whiteTheme = {
+"colors": {
+"frame": "white",
+"bookmark_text": "black",
+"popup": "white",
+"popup_text": "black",
+"popup_border": "black",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "white",
+"sidebar_text": "black",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "black",
+"tab_text": "black",
+"tab_line": "blue",
+"tab_loading": "white",
+"tab_background_text": "black",
+"toolbar_field": "white",
+"toolbar_field_text": "black",
+"toolbar_field_highlight": "blue",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "white",
+"toolbar_field_text_focus": "black",
+"toolbar_field_border": "black",
+"toolbar_field_border_focus": "red",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "white",
+"toolbar": "white"
+        }
+    }
+browser.theme.update(whiteTheme);
+});
+
+formTwo.addEventListener('click', function () {
+var blackTheme = {
+"colors": {
+"frame": "black",
+"bookmark_text": "white",
+"popup": "#303030",
+"popup_text": "white",
+"popup_border": "gray",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "#303030",
+"sidebar_text": "white",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "gray",
+"tab_text": "white",
+"tab_line": "blue",
+"tab_loading": "#303030",
+"tab_background_text": "white",
+"toolbar_field": "#303030",
+"toolbar_field_text": "white",
+"toolbar_field_highlight": "blue",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "#303030",
+"toolbar_field_text_focus": "white",
+"toolbar_field_border": "gray",
+"toolbar_field_border_focus": "red",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "#303030",
+"toolbar": "#303030"
+        }
+    }
+browser.theme.update(blackTheme);
+});
+
+formThree.addEventListener('click', function () {
+var blueTheme = {
+"colors": {
+"frame": "LightBlue",
+"bookmark_text": "black",
+"popup": "#cceeff",
+"popup_text": "black",
+"popup_border": "black",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "white",
+"sidebar_text": "black",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "black",
+"tab_text": "black",
+"tab_line": "blue",
+"tab_loading": "#cceeff",
+"tab_background_text": "black",
+"toolbar_field": "#cceeff",
+"toolbar_field_text": "black",
+"toolbar_field_highlight": "blue",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "#cceeff",
+"toolbar_field_text_focus": "black",
+"toolbar_field_border": "black",
+"toolbar_field_border_focus": "red",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "#cceeff",
+"toolbar": "#cceeff"
+        }
+    }
+browser.theme.update(blueTheme);
+});
+
+formFour.addEventListener('click', function () {
+var greenTheme = {
+"colors": {
+"frame": "LightGreen",
+"bookmark_text": "black",
+"popup": "#d0f0c0",
+"popup_text": "black",
+"popup_border": "black",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "white",
+"sidebar_text": "black",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "black",
+"tab_text": "black",
+"tab_line": "blue",
+"tab_loading": "#d0f0c0",
+"tab_background_text": "black",
+"toolbar_field": "#d0f0c0",
+"toolbar_field_text": "black",
+"toolbar_field_highlight": "blue",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "#d0f0c0",
+"toolbar_field_text_focus": "black",
+"toolbar_field_border": "black",
+"toolbar_field_border_focus": "red",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "#d0f0c0",
+"toolbar": "#d0f0c0"
+        }
+    }
+browser.theme.update(greenTheme);
+});
+
+formFive.addEventListener('click', function () {
+var yellowTheme = {
+"colors": {
+"frame": "Gold",
+"bookmark_text": "black",
+"popup": "#ffeecc",
+"popup_text": "black",
+"popup_border": "black",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "white",
+"sidebar_text": "black",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "black",
+"tab_text": "black",
+"tab_line": "blue",
+"tab_loading": "#ffeecc",
+"tab_background_text": "black",
+"toolbar_field": "#ffeecc",
+"toolbar_field_text": "black",
+"toolbar_field_highlight": "blue",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "#ffeecc",
+"toolbar_field_text_focus": "black",
+"toolbar_field_border": "black",
+"toolbar_field_border_focus": "red",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "#ffeecc",
+"toolbar": "#ffeecc"
+        }
+    }
+browser.theme.update(yellowTheme);
+});
+
+formSix.addEventListener('click', function () {
+var redTheme = {
+"colors": {
+"frame": "Pink",
+"bookmark_text": "black",
+"popup": "MistyRose",
+"popup_text": "black",
+"popup_border": "black",
+"popup_highlight": "blue",
+"popup_highlight_text": "white",
+"sidebar": "white",
+"sidebar_text": "black",
+"sidebar_highlight": "blue",
+"sidebar_highlight_text": "white",
+"sidebar_border": "black",
+"tab_text": "black",
+"tab_line": "blue",
+"tab_loading": "MistyRose",
+"tab_background_text": "black",
+"toolbar_field": "MistyRose",
+"toolbar_field_text": "black",
+"toolbar_field_highlight": "red",
+"toolbar_field_highlight_text": "white",
+"toolbar_field_focus": "MistyRose",
+"toolbar_field_text_focus": "black",
+"toolbar_field_border": "black",
+"toolbar_field_border_focus": "blue",
+"toolbar_top_separator": "black",
+"toolbar_bottom_separator": "black",
+"toolbar_vertical_separator": "black",
+"tab_selected": "MistyRose",
+"toolbar": "MistyRose"
+        }
+    }
+browser.theme.update(redTheme);
+});
